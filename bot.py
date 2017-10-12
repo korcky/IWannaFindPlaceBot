daeeffe2dd972692b60b578953f6a8f370ca0b77
Sergey Zhirnov <korckykrou@gmail.com>
2017-10-13 00:43:52 +0300

    Refactor bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,16 @@ from telegram.ext import CommandHandler
 def start_bot(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="Добро пожаловать!")
 
-updater = Updater(config.TOKEN)
-start_handler = CommandHandler('start', start_bot) #Реакция на команду /start: вывод приветственного сообщения.
+    
+def launch(token):
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    
+    start_handler = CommandHandler('start', start_bot) #Реакция на команду /start: вывод приветственного сообщения.
 
-updater.dispatcher.add_handler(start_handler) #Создание обработчика.
-updater.start_polling() #Запуск бота.
+    dispatcher.add_handler(start_handler) #Создание обработчика.
+    updater.start_polling() #Запуск бота.
+
+
+if __name__ == '__main__':
+    launch(token=config.TOKEN)
