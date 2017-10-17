f84db9c1c38b36a36d8b2244bb1cce30f7fef099
korcky <korckykrou@gmail.com>
2017-10-17 23:24:59 +0300

    Fix inline handler * second menu works rework * refactor repository

--- bot.py
+++ bot.py
@@ -4,14 +4,15 @@ from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start_bot(message):
     bot.send_message(message.chat.id, 'Hi!')
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Моя геолокация', 'Станция метро']])
-    res=bot.send_message(message.chat.id, 'Выбери способ поиска',
+    bot.send_message(message.chat.id, 'Выбери способ поиска',
                      reply_markup=keyboard)
-    #bot.register_next_step_handler(res, name)
+
 
 @bot.callback_query_handler(func=lambda c: True)
 def inline(c):
@@ -35,50 +36,43 @@ def inline(c):
             parse_mode='Markdown',
             reply_markup=keyboard
         )
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        if c.data == 'Линия 1':
+            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
+                           ['Девяткино', 'Гражданский проспект', 'Академическая', 'Политехническая', 'Площадь Мужества',
+                            'Лесная', 'Выборгская', 'Площадь Ленина', 'Чернышевская', 'Площадь Восстания',
+                            'Владимирская', 'Пушкинская', 'Технологический институт', 'Балтийская', 'Нарвская',
+                            'Кировский завод', 'Автово', 'Ленинский просппект', 'Проспект Ветеранов']])
+        elif c.data == 'Линия 2':
+            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
+                           ['Парнас', 'Проспект Просвещения', 'Озерки', 'Удельная', 'Пионерская',
+                            'Черная речка', 'Петроградская', 'Горьковская', 'Невский проспект',
+                            'Сенная площадь', 'Технологический институт', 'Фрунзенская',
+                            'Московские ворота', 'Электросила', 'Парк Победы', 'Московская',
+                            'Звездная', 'Купчино']])
+        elif c.data == 'Линия 3':
+            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
+                           ['Приморская', 'Василеостровская', 'Гостиный двор', 'Маяковская',
+                            'Площадь А.Невского', 'Елизаровская', 'Ломоносовская', 'Пролетарская',
+                            'Обухово', 'Рыбацкое']])
+        elif c.data == 'Линия 4':
+            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
+                           ['Спасская', 'Достоевская', 'Лиговский проспект', 'Новочеркасская',
+                            'Ладожская', 'Проспект Большевиков', 'Улица Дыбенко']])
+        elif c.data == 'Линия 5':
+            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
+                           ['Комендантский проспект', 'Старая Деревня', 'Крестовский остров',
+                            'Чкаловская', 'Спортивная', 'Адмиралтейская', 'Садовая', 'Звенигородская',
+                            'Обводный канал', 'Волковская', 'Бухарестская', 'Международная']])
 
-        @bot.callback_query_handler(func=lambda branch: True)
-        def inline_branches(branch):
-            keyboard1 = types.InlineKeyboardMarkup()
-            if branch.data == 'Линия 1':
-                #keyboard1 = types.InlineKeyboardMarkup()
-                keyboard1.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                       ['Девяткино', 'Гражданский проспект','Академическая', 'Политехническая', 'Площадь Мужества',
-                        'Лесная', 'Выборгская', 'Площадь Ленина', 'Чернышевская', 'Площадь Восстания',
-                        'Владимирская', 'Пушкинская', 'Технологический институт', 'Балтийская', 'Нарвская',
-                        'Кировский завод', 'Автово', 'Ленинский просппект', 'Проспект Ветеранов']])
-            elif branch.data == 'Линия 2':
-                #keyboard1 = types.InlineKeyboardMarkup()
-                keyboard1.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                      ['Парнас', 'Проспект Просвещения', 'Озерки', 'Удельная', 'Пионерская',
-                       'Черная речка', 'Петроградская', 'Горьковская', 'Невский проспект',
-                       'Сенная площадь', 'Технологический институт', 'Фрунзенская',
-                       'Московские ворота', 'Электросила', 'Парк Победы', 'Московская',
-                       'Звездная', 'Купчино']])
-            elif branch.data == 'Линия 3':
-                #keyboard1 = types.InlineKeyboardMarkup()
-                keyboard1.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                      ['Приморская', 'Василеостровская', 'Гостиный двор', 'Маяковская',
-                       'Площадь А.Невского', 'Елизаровская', 'Ломоносовская', 'Пролетарская',
-                       'Обухово', 'Рыбацкое']])
-            elif branch.data == 'Линия 4':
-                #keyboard1 = types.InlineKeyboardMarkup()
-                keyboard1.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                      ['Спасская', 'Достоевская', 'Лиговский проспект', 'Новочеркасская',
-                       'Ладожская', 'Проспект Большевиков', 'Улица Дыбенко']])
-            elif branch.data == 'Линия 5':
-                #keyboard1 = types.InlineKeyboardMarkup()
-                keyboard1.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                      ['Комендантский проспект', 'Старая Деревня', 'Крестовский остров',
-                       'Чкаловская', 'Спортивная', 'Адмиралтейская', 'Садовая', 'Звенигородская',
-                       'Обводный канал', 'Волковская', 'Бухарестская', 'Международная']])
-
-            bot.edit_message_text(
-                chat_id=branch.message.chat.id,
-                message_id=branch.message.message_id,
-                text='Выберите станцию',
-                parse_mode='Markdown',
-                reply_markup=keyboard1
-            )
+        bot.edit_message_text(
+            chat_id=c.message.chat.id,
+            message_id=c.message.message_id,
+            text='Выберите станцию',
+            parse_mode='Markdown',
+            reply_markup=keyboard
+        )
 
 
 if __name__ == '__main__':
