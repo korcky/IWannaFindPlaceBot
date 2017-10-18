2af3db5ab5682807b328c86b0e811a3c0410a4a0
korcky <korckykrou@gmail.com>
2017-10-18 17:56:04 +0300

    Refacore code and repository * refactore code to work with python-telegram-bot module * update .gitignore * add requirements.txt

--- bot.py
+++ bot.py
@@ -1,79 +1,59 @@
 import config
-import telebot
-from telebot import types
 
-bot = telebot.TeleBot(config.TOKEN)
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-@bot.message_handler(commands=['start'])
-def start_bot(message):
-    bot.send_message(message.chat.id, 'Hi!')
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Моя геолокация', 'Станция метро']])
-    bot.send_message(message.chat.id, 'Выбери способ поиска',
-                     reply_markup=keyboard)
-
-
-@bot.callback_query_handler(func=lambda c: True)
-def inline(c):
-
-    if c.data == 'Моя геолокация':
-        bot.edit_message_text(
-            chat_id=c.message.chat.id,
-            message_id=c.message.message_id,
-            text='Мы работаем над этим;)',
-            parse_mode='Markdown'
-        )
-
-    elif c.data == 'Станция метро':
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                       ['Линия 1', 'Линия 2','Линия 3', 'Линия 4', 'Линия 5']])
-        bot.edit_message_text(
-            chat_id=c.message.chat.id,
-            message_id=c.message.message_id,
-            text='Выберите ветку',
-            parse_mode='Markdown',
-            reply_markup=keyboard
-        )
-    else:
-        keyboard = types.InlineKeyboardMarkup()
-        if c.data == 'Линия 1':
-            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                           ['Девяткино', 'Гражданский проспект', 'Академическая', 'Политехническая', 'Площадь Мужества',
-                            'Лесная', 'Выборгская', 'Площадь Ленина', 'Чернышевская', 'Площадь Восстания',
-                            'Владимирская', 'Пушкинская', 'Технологический институт', 'Балтийская', 'Нарвская',
-                            'Кировский завод', 'Автово', 'Ленинский просппект', 'Проспект Ветеранов']])
-        elif c.data == 'Линия 2':
-            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                           ['Парнас', 'Проспект Просвещения', 'Озерки', 'Удельная', 'Пионерская',
-                            'Черная речка', 'Петроградская', 'Горьковская', 'Невский проспект',
-                            'Сенная площадь', 'Технологический институт', 'Фрунзенская',
-                            'Московские ворота', 'Электросила', 'Парк Победы', 'Московская',
-                            'Звездная', 'Купчино']])
-        elif c.data == 'Линия 3':
-            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                           ['Приморская', 'Василеостровская', 'Гостиный двор', 'Маяковская',
-                            'Площадь А.Невского', 'Елизаровская', 'Ломоносовская', 'Пролетарская',
-                            'Обухово', 'Рыбацкое']])
-        elif c.data == 'Линия 4':
-            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                           ['Спасская', 'Достоевская', 'Лиговский проспект', 'Новочеркасская',
-                            'Ладожская', 'Проспект Большевиков', 'Улица Дыбенко']])
-        elif c.data == 'Линия 5':
-            keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in
-                           ['Комендантский проспект', 'Старая Деревня', 'Крестовский остров',
-                            'Чкаловская', 'Спортивная', 'Адмиралтейская', 'Садовая', 'Звенигородская',
-                            'Обводный канал', 'Волковская', 'Бухарестская', 'Международная']])
-
-        bot.edit_message_text(
-            chat_id=c.message.chat.id,
-            message_id=c.message.message_id,
-            text='Выберите станцию',
-            parse_mode='Markdown',
-            reply_markup=keyboard
-        )
+main_kb = InlineKeyboardMarkup([[InlineKeyboardButton('Geolocation', callback_data='geo'),
+                                 InlineKeyboardButton('Metro station', callback_data='ms')]])
+lines_kb = InlineKeyboardMarkup([[InlineKeyboardButton('Line 1', callback_data='red'),
+                                  InlineKeyboardButton('Line 2', callback_data='blue')]])
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+def start(bot, update):
+    update.message.reply_text('Please choose method:', reply_markup=main_kb)
+
+
+def geolocation(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text='There\'s nothing here right now.',
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+
+def metro_station(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text='Please choose line:',
+                          reply_markup=lines_kb,
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+
+def help(bot, update):
+    update.message.reply_text('Use /start to use this bot.')
+
+
+def error(bot, update, error):
+    logging.warning('Update {} caused error {}'.format(update, error))
+
+
+# Create the Updater and pass it your bot's token.
+updater = Updater(config.TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(geolocation, pattern='geo'))
+updater.dispatcher.add_handler(CallbackQueryHandler(metro_station, pattern='ms'))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_error_handler(error)
+
+# Start the Bot
+updater.start_polling()
+
+# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT
+updater.idle()
