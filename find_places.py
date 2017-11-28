d531ce3f3c6892492c6c04ed0a3d71beb1f3cf44
Sergey Zhirnov <korckykrou@gmail.com>
2017-11-28 21:33:52 +0300

    Fix

--- find_places.py
+++ find_places.py
@@ -9,6 +9,8 @@ client = Client(key=config.API_KEY)
 def find_places(user_location, user_radius):
     if type(user_location) == dict:
         location = (user_location['latitude'], user_location['longitude'])
+    else:
+        location = user_location
     result = places.places_nearby(client, location=location, radius=user_radius, type='bar') #keyword='cruise')
     #opening_hours = result['opening hours']
     #open_now = opening_hours['open now']
